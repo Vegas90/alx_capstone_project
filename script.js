7815acd759a9e9122744f066846feaf6d7831651
Vegas90 <amosmwoni@gmail.com>
2023-10-26 10:16:24 +0300

    M

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function toggleMenu(){
+// Define a function called toggleMenu
+function toggleMenu() {
+    // Select the HTML element with the class "menu-links" and store it in the 'menu' variable
     const menu = document.querySelector(".menu-links");
+    
+    // Select the HTML element with the class "hamburger-icon" and store it in the 'icon' variable
     const icon = document.querySelector(".hamburger-icon");
+    
+    // Toggle the "open" class on the 'menu' element, which controls its visibility
     menu.classList.toggle("open");
+    
+    // Toggle the "open" class on the 'icon' element, which typically changes the hamburger menu icon
     icon.classList.toggle("open");
 }
+
